2fb4038b9538bed3da748f7c7cfa1580d336033c
Евгений <ieugenor@gmail.com>
2014-10-20 00:55:37 +0400

    Сделал лаяут ND. ???

--- gen/ru/mstuca/R.java
+++ gen/ru/mstuca/R.java
@@ -30,23 +30,27 @@ public final class R {
     public static final class drawable {
         public static final int drawer_shadow=0x7f020000;
         public static final int ic_action_about=0x7f020001;
-        public static final int ic_action_group=0x7f020002;
-        public static final int ic_action_search=0x7f020003;
-        public static final int ic_action_setting=0x7f020004;
-        public static final int ic_action_teacher=0x7f020005;
-        public static final int ic_drawer=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
+        public static final int ic_action_about_white=0x7f020002;
+        public static final int ic_action_group=0x7f020003;
+        public static final int ic_action_search=0x7f020004;
+        public static final int ic_action_search_white=0x7f020005;
+        public static final int ic_action_setting=0x7f020006;
+        public static final int ic_action_settings_white=0x7f020007;
+        public static final int ic_action_teacher=0x7f020008;
+        public static final int ic_drawer=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
     }
     public static final class id {
-        public static final int action_example=0x7f08000a;
-        public static final int action_settings=0x7f080009;
+        public static final int action_example=0x7f08000b;
+        public static final int action_settings=0x7f08000a;
+        public static final int button1=0x7f080006;
         public static final int container=0x7f080001;
         public static final int drawer_layout=0x7f080000;
-        public static final int editText1=0x7f080006;
+        public static final int editText1=0x7f080007;
         public static final int navigation_drawer=0x7f080002;
         public static final int section_label=0x7f080003;
-        public static final int tab1=0x7f080007;
-        public static final int tab2=0x7f080008;
+        public static final int tab1=0x7f080008;
+        public static final int tab2=0x7f080009;
         public static final int tvItem=0x7f080005;
         public static final int tvSelectionHeader=0x7f080004;
     }
@@ -58,8 +62,9 @@ public final class R {
         public static final int navigation_drawer_item=0x7f030004;
         public static final int navigation_drawer_layout=0x7f030005;
         public static final int navigation_drawer_layout_blue=0x7f030006;
-        public static final int search_fragment=0x7f030007;
-        public static final int search_fragment_tabs=0x7f030008;
+        public static final int navigation_drawer_layout_dark=0x7f030007;
+        public static final int search_fragment=0x7f030008;
+        public static final int search_fragment_tabs=0x7f030009;
     }
     public static final class menu {
         public static final int global=0x7f070000;
--- src/ru/mstuca/navigationdrawer/NavigationDrawerFragment.java
+++ src/ru/mstuca/navigationdrawer/NavigationDrawerFragment.java
@@ -93,18 +93,20 @@ public class NavigationDrawerFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		mDrawerListView = (ListView) inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
-		mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				selectItem(position);
-			}
-		});
-		mDrawerListView.setAdapter(new ArrayAdapter<String>(getActionBar().getThemedContext(), android.R.layout.simple_list_item_activated_1,
-				android.R.id.text1, new String[] { getString(R.string.title_section1), getString(R.string.title_section2),
-						getString(R.string.title_section3), }));
-		mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
-		return mDrawerListView;
+		// mDrawerListView = (ListView) inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+		// mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		// @Override
+		// public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		// selectItem(position);
+		// }
+		// });
+		// mDrawerListView.setAdapter(new ArrayAdapter<String>(getActionBar().getThemedContext(), android.R.layout.simple_list_item_activated_1,
+		// android.R.id.text1, new String[] { getString(R.string.title_section1), getString(R.string.title_section2),
+		// getString(R.string.title_section3), }));
+		// mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
+		//return mDrawerListView;
+		
+		return inflater.inflate(R.layout.navigation_drawer_layout_blue, container, false);
 	}
 
 	public boolean isDrawerOpen() {
